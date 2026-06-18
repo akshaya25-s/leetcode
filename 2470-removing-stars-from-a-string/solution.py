8268489e051a8f0a78b4129b04ea5a6b8b55ead5
akshaya25-s <nityasree.2516@gmail.com>
2026-06-18 04:33:12 +0000

    Sync LeetCode submission Runtime - 95 ms (48.57%), Memory - 20.6 MB (19.07%)

---
+++ 2470-removing-stars-from-a-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stk=[]
+        for i in s:
+            if i=='*':
+                stk.pop()
+            else:
+                stk.append(i)
+        return ''.join(stk)
