4f001d5f977c0cbf73d4e638eeaf7572717cc4fb
akshaya25-s <nityasree.2516@gmail.com>
2026-08-03 08:18:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (61.36%)

---
+++ 0198-house-robber/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
+        p=nums[0]
+        c=max(nums[0],nums[1])
+        for i in range(2,n):
+            p,c=c,max(p+nums[i],c)
+        return c
