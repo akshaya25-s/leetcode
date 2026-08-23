00ced62b85a12fd4fa53cb6ba1b010e75764b10c
akshaya25-s <nityasree.2516@gmail.com>
2026-08-23 12:10:41 +0530

    Time: 7 ms (19.57%) | Memory: 19.3 MB (84.27%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        for num in arr2:
+            result.extend([num] * arr1.count(num))
+        l = [num for num in arr1 if num not in arr2]
+        result.extend(sorted(l))
+        return result
+
