863224f7bd1ec423ccb7f48adbd6629ac051e1bb
akshaya25-s <nityasree.2516@gmail.com>
2026-09-08 13:55:33 +0530

    Time: 5 ms (14.01%) | Memory: 19.3 MB (42.93%) - LeetSync

---
+++ 4378-nearest-available-drone/nearest-available-drone.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nearestDrone(self, drones: list[list[int]], target: list[int]) -> int:
+        n = -1
+        m = float('inf')
+        for i, (x, y, r) in enumerate(drones):
+            dx = abs(x - target[0])
+            dy = abs(y - target[1])
+            dist = dx + dy 
+            
+            if dist <= r and dist < m:
+                m = dist
+                n = i
+        
+        return n
+
+
